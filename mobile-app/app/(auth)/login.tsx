5ba5b3aa30985a03784845214f201a93fc249c11
Eugene <eugyneehunter@gmail.com>
2026-03-29 12:38:44 +0300

    feat: initialize mobile app with Expo, add reset script, and configure TypeScript

---
+++ mobile-app/app/(auth)/login.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <View>
+            <Text>login page here</Text>
+        </View>
+    )
+}
+
+export default LoginPage
